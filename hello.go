20597d29c94f864940467c03c306b6442793b35f
Jaewook Ahn <ajw4586@gmail.com>
2020-06-06 20:14:19 +0900

    Add basics I learned

--- hello.go
+++ hello.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
+import "math/rand"
+
+func add(a, b, c int) int {
+    return a + b + c
+}
+
+func swap(str1, str2 string) (string, string) {
+    return str2, str1
+}
+
+var is_go bool = true
+// wrong usage
+// id := 1213
 
 func main() {
-    fmt.Printf("Hello world!\n")
+    fmt.Println(swap("world!", "Hello"))
+    fmt.Println("THe current time is", time.Now())
+    fmt.Println("Random number is", rand.Intn(10))
+    fmt.Println("Simple add instruction: 1 + 2 + 3 =", add(1, 2, 3))
+
+    id := 1213
+    fmt.Println("Test variables is_go:", is_go, "id:", id)
 }
